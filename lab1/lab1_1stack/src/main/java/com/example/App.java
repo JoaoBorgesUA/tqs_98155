25e2d0dff123a801de2c2bcfaaac570b93e3047c
JoaoBorgesUA <borgesjps@ua.pt>
2022-03-17 16:39:21 +0000

    1.1 done

--- lab1/lab1_1stack/src/main/java/com/example/App.java
+++ lab1/lab1_1stack/src/main/java/com/example/App.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 /**
  * Hello world!
@@ -15,5 +16,27 @@ public class App<T> {
 
     public void push(T val){
         this.stack.add(val);
+    }
+
+    public T pop(){
+        if (stack.size()==0){
+            throw new NoSuchElementException();
+        }
+        return this.stack.remove(this.stack.size()-1);
+    }
+
+    public T peek(){
+        if (stack.size()==0){
+            throw new NoSuchElementException();
+        }
+        return this.stack.get(this.stack.size()-1);
+    }
+
+    public int size(){
+        return this.stack.size();
+    }
+
+    public boolean isEmpty(){
+        return this.stack.isEmpty();
     }
 }
--- lab1/lab1_1stack/src/test/java/com/example/AppTest.java
+++ lab1/lab1_1stack/src/test/java/com/example/AppTest.java
@@ -1,19 +1,21 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     private App<Integer> stack;
 
     @BeforeEach
@@ -25,5 +27,65 @@ public class AppTest
     @Test
     void EmptyAtStart(){
         assertTrue(stack.isEmpty());
+    }
+
+    @DisplayName("Stack size 0 at the beggining")
+    @Test
+    void SizeZeroAtStart(){
+        assertTrue(stack.size()==0);
+    }
+
+    @DisplayName("Stack is being correctly filled up")      //what??
+    @Test
+    void IsPushed(){
+        assertTrue(stack.size()==0);
+    }
+
+    @DisplayName("Stack can correctly pop")
+    @Test
+    void PopAnElement(){
+        int i = 123;
+        stack.push(i);
+        assertEquals(i, stack.pop());
+    }
+
+    @DisplayName("Stack can correctly peek")
+    @Test
+    void PeekAnElement(){
+        int i = 123;
+        stack.push(i);
+        stack.push(124);
+        assertEquals(124, stack.peek());
+        assertEquals(124, stack.peek());
+    }
+
+    @DisplayName("Stack empty after n pops")
+    @Test
+    void EmptyTheStack(){
+        for (int i = 1; i < 4; i++) {
+            stack.push(i);
+            System.out.print(i);
+        }
+        for (int i = 0; i<3; i++){
+            stack.pop();
+        }
+
+        assertTrue(stack.isEmpty());
+    }
+
+    @DisplayName("When stack is empty it shows the exception")
+    @Test
+    void StackPopThrowsException(){
+        assertThrows(NoSuchElementException.class, () -> {
+            stack.pop();
+        });
+    }
+
+    @DisplayName("When stack is empty it shows the exception")
+    @Test
+    void StackPeekThrowsException(){
+        assertThrows(NoSuchElementException.class, () -> {
+            stack.peek();
+        });
     }
 }

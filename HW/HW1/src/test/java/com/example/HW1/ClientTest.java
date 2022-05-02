ab85962c8beab7ac384e153d5d101af85e9e2201
JoaoBorgesUA <borgesjps@ua.pt>
2022-05-02 23:35:53 +0100

    what??

--- HW/HW1/src/test/java/com/example/HW1/ClientTest.java
+++ HW/HW1/src/test/java/com/example/HW1/ClientTest.java
@@ -1,6 +1,7 @@
 package com.example.HW1;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
